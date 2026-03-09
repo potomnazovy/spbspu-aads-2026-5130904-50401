77ad37558e6f80fcd1e015fdc586b55ff1623ef4
potomnazovy <skeleton39876@mail.ru>
2026-03-09 16:32:25 +0300

    add construct List boost tests

--- vasyakin.kirill/S1/test-list.cpp
+++ vasyakin.kirill/S1/test-list.cpp
@@ -2,8 +2,19 @@
 #include "list.hpp"
 
 BOOST_AUTO_TEST_SUITE(ListTests)
+BOOST_AUTO_TEST_CASE(DefConstructTest)
 {
+  vasyakin::List< int > list;
+  BOOST_CHECK_EQUAL(list.get_size(), 0);
+  BOOST_CHECK(list.begin() == list.end());
+}
 
+BOOST_AUTO_TEST_CASE(ValConstructTest)
+{
+  vasyakin::List< int > list(52);
+  BOOST_CHECK_EQUAL(list.get_size(), 1);
+  BOOST_CHECK(list.begin() != list.end());
+  BOOST_CHECK_EQUAL(*list.begin(), 52);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
