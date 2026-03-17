a6a1f74a4da93b0fb769b8fd13ecd5b0ecddd6d3
potomnazovy <skeleton39876@mail.ru>
2026-03-17 22:45:20 +0300

    add arithmetic func

--- vasyakin.kirill/S2/math_op.cpp
+++ vasyakin.kirill/S2/math_op.cpp
@@ -75,3 +75,117 @@ int vasyakin::getPrecedence(const std::string& op)
   }
   return 0;  
 }
+
+long long gcd(long long a, long long b)
+{
+  a = std::abs(a);
+  b = std::abs(b);
+  while (b != 0)
+  {
+    long long temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+long long lcm(long long a, long long b)
+{
+  if (a == 0 || b == 0)
+  {
+    throw std::runtime_error("LCM with zero is undefined");
+  }
+  long long g = gcd(a, b);
+  long long abs_a = std::abs(a);
+  long long abs_b = std::abs(b);
+
+  if (abs_a / g > vasyakin::MAX / abs_b)
+  {
+    throw std::overflow_error("LCM overflow");
+  }
+  return (abs_a / g) * abs_b;
+}
+
+long long sum(long long a, long long b)
+{
+  if (b > 0 && a > vasyakin::MAX - b)
+  {
+    throw std::overflow_error("Sum overflow");
+  }
+  else if (b < 0 && a < vasyakin::MIN - b)
+  {
+    throw std::overflow_error("Sum underflow");
+  }
+  return a + b;
+}
+
+long long subtract(long long a, long long b)
+{
+  if (b > 0 && a < vasyakin::MIN + b)
+  {
+    throw std::overflow_error("Subtract underflow");
+  }
+  else if (b < 0 && a > vasyakin::MAX + b)
+  {
+    throw std::overflow_error("Subtract overflow");
+  }
+  return a - b;
+}
+
+long long multiply(long long a, long long b)
+{
+  if (a == 0 || b == 0)
+  {
+    return 0;
+  }
+
+  if (a > 0)
+  {
+    if (b > 0 && a > vasyakin::MAX / b)
+    {
+      throw std::overflow_error("Mult overflow");
+    }
+    else if (b < 0 && b < vasyakin::MIN / a)
+    {
+      throw std::overflow_error("Mult underflow");
+    }
+  }
+  else
+  {
+    if (b > 0 && a < vasyakin::MIN / b)
+    {
+      throw std::overflow_error("Mult underflow");
+    }
+    else if (b < 0 && a < vasyakin::MAX / b)
+    {
+      throw std::overflow_error("Mult overflow");
+    }
+  }
+  return a * b;
+}
+
+long long divide(long long a, long long b)
+{
+  if (b == 0)
+  {
+    throw std::runtime_error("Division by zero");
+  }
+
+  if (a == vasyakin::MIN && b == -1)
+  {
+    throw std::overflow_error("Division overflow");
+  }
+  return a / b;
+}
+
+long long mod(long long a, long long b)
+{
+  if (b == 0)
+  {
+    throw std::runtime_error("Division by zero");
+  }
+  else
+  {
+    return (a % b + b) % b;
+  }
+}
