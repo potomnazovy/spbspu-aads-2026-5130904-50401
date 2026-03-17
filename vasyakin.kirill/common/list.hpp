e1921b3c7e677fa77a940463324cfe5311dd8af0
potomnazovy <skeleton39876@mail.ru>
2026-03-17 22:31:20 +0300

    Move List.hpp to common folder for shared usage

--- vasyakin.kirill/common/list.hpp
+++ vasyakin.kirill/common/list.hpp
@@ -1,66 +1,14 @@
 #ifndef LIST_HPP
 #define LIST_HPP
-
+#include "iterators.hpp"
+#include "node.hpp"
 #include <cstddef>
 #include <utility>
 #include <limits>
 
 namespace vasyakin
 {
-  template< class T > class Node;
-  template< class T > class List;
   const size_t MAX = std::numeric_limits< size_t >::max();
-
-  template< class T >
-  class LIter
-  {
-  private:
-    friend class List< T >;
-    Node< T >* ptr;
-    explicit LIter(Node< T >* p);
-
-  public:
-    T& operator*() const;
-    T* operator->() const;
-    LIter& operator++();
-    LIter operator++(int);
-    bool operator==(const LIter& other) const;
-    bool operator!=(const LIter& other) const;
-    Node< T >* get_ptr() const;
-  };
-
-  template< class T >
-  class LCIter
-  {
-  private:
-    friend class List< T >;
-    const Node< T >* ptr;
-    explicit LCIter(const Node< T >* p);
-    explicit LCIter(const LIter< T >& it);
-
-  public:
-    const T& operator*() const;
-    const T* operator->() const;
-    LCIter& operator++();
-    LCIter operator++(int);
-    bool operator==(const LCIter& other) const;
-    bool operator!=(const LCIter& other) const;
-    const Node< T >* get_ptr() const;
-  };
-
-  template< class T >
-  class Node
-  {
-  private:
-    T val;
-    Node< T >* next;
-    friend class List< T >;
-    friend class LIter< T >;
-    friend class LCIter< T >;
-
-  public:
-    explicit Node(const T& value);
-  };
 
   template< class T >
   class List
@@ -95,117 +43,6 @@ namespace vasyakin
     Node< T >* get_first() const;
     void clear();
   };
-
-  template< class T >
-  LIter< T >::LIter(Node< T >* p):
-    ptr(p)
-  {}
-
-  template< class T >
-  T& LIter< T >::operator*() const
-  {
-    return ptr->val;
-  }
-
-  template< class T >
-  T* LIter< T >::operator->() const
-  {
-    return &(ptr->val);
-  }
-
-  template< class T >
-  LIter< T >& LIter< T >::operator++()
-  {
-    ptr = ptr->next;
-    return *this;
-  }
-
-  template< class T >
-  LIter< T > LIter< T >::operator++(int)
-  {
-    LIter< T > tmp = *this;
-    ptr = ptr->next;
-    return tmp;
-  }
-
-  template< class T >
-  bool LIter< T >::operator==(const LIter& other) const
-  {
-    return ptr == other.ptr;
-  }
-
-  template< class T >
-  bool LIter< T >::operator!=(const LIter& other) const
-  {
-    return ptr != other.ptr;
-  }
-
-  template< class T >
-  Node< T >* LIter< T >::get_ptr() const
-  {
-    return ptr;
-  }
-
-  template< class T >
-  LCIter< T >::LCIter(const Node< T >* p):
-    ptr(p)
-  {}
-
-  template< class T >
-  LCIter< T >::LCIter(const LIter< T >& it):
-    ptr(it.ptr)
-  {}
-
-  template< class T >
-  const T& LCIter< T >::operator*() const
-  {
-    return ptr->val;
-  }
-
-  template< class T >
-  const T* LCIter< T >::operator->() const
-  {
-    return &(ptr->val);
-  }
-
-  template< class T >
-  LCIter< T >& LCIter< T >::operator++()
-  {
-    ptr = ptr->next;
-    return *this;
-  }
-
-  template< class T >
-  LCIter< T > LCIter< T >::operator++(int)
-  {
-    LCIter< T > tmp = *this;
-    ptr = ptr->next;
-    return tmp;
-  }
-
-  template< class T >
-  bool LCIter< T >::operator==(const LCIter& other) const
-  {
-    return ptr == other.ptr;
-  }
-
-  template< class T >
-  bool LCIter< T >::operator!=(const LCIter& other) const
-  {
-    return ptr != other.ptr;
-  }
-
-  template< class T >
-  const Node< T >* LCIter< T >::get_ptr() const
-  {
-    return ptr;
-  }
-
-  template< class T >
-  Node< T >::Node(const T& value):
-    val(value),
-    next(nullptr)
-  {}
 
   template< class T >
   List< T >::List():
---
+++ vasyakin.kirill/common/node.hpp
@@ -0,0 +1,27 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+namespace vasyakin
+{
+  template< class T >
+  class Node
+  {
+  private:
+    T val;
+    Node< T >* next;
+    friend class List< T >;
+    friend class LIter< T >;
+    friend class LCIter< T >;
+
+  public:
+    explicit Node(const T& value);
+  };
+  
+  template< class T >
+  Node< T >::Node(const T& value):
+    val(value),
+    next(nullptr)
+  {}
+}
+
+#endif
