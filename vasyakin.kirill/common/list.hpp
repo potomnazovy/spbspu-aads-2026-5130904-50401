63763fcae0f1a3b742e319921afc59bba4edcfc8
potomnazovy <skeleton39876@mail.ru>
2026-03-18 15:53:35 +0300

    add template in namespace

--- vasyakin.kirill/common/list.hpp
+++ vasyakin.kirill/common/list.hpp
@@ -7,9 +7,7 @@
 
 namespace vasyakin
 {
-
-  const long long MAX = std::numeric_limits< long long >::max();
-
+  
   template< class T >
   class List
   {
--- vasyakin.kirill/common/node.hpp
+++ vasyakin.kirill/common/node.hpp
@@ -1,18 +1,20 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
-template< class T > class List;
-template< class T > class LIter;
-template< class T > class LCIter;
-
 namespace vasyakin
 {
+
+  template< class T > class List;
+  template< class T > class LIter;
+  template< class T > class LCIter;
+
   template< class T >
   class Node
   {
   private:
     T val;
     Node< T >* next;
+
     friend class List< T >;
     friend class LIter< T >;
     friend class LCIter< T >;
